54a116a53450dc8cdf6f3d1512cf8ea5b8cdebfc
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-11-10 16:23:44 -0800

    setting up the server to start running on the port

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const dbConnection = require('');// mongoDb database will go here once it has been established
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// db connection code goes here
+
+// db error connection goesw here
+
+//server listening goes here with midleware
+Server.start().then(() => {
+    Server.applyMiddleware({ app });
+
+    if(process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, )));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '..client/dist/index.html'));
+        });
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server is running and listening to ${PORT}`);
+        console.log(`server is listening to database`);
+    });
+});
