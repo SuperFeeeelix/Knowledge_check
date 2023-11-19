e01be615c52b9e53a5c6ba7d64c33223fcf8d40f
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-11-18 22:01:17 -0800

    added files with the signin.html and also added code for the databse

---
+++ server/config/cleanDB.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const db = require('')// change into the connection.js file 
+
+module.exports = async () => {
+    try {
+        const databseName = ''// database name will go here 
+        const collectionNames = []// databse names will also go here 
+
+        const dbConnection = mongoose.connection.useDB(databaseName);
+
+        for (const collectionName of collectionNames) {
+            const collection = dbConnection.collection(collectionName);
+            const collectionInfo = await collection.listIndexs().toArray();
+
+            if (collectionInfo.length) {
+
+                await collection.drop();
+                console.lof(`Collection ${collectionName} in databse ${databseName} dropped.`);
+
+            } else {
+                console.log(`Collection &{collectionName} in databse ${ddatabseName} not found.`);
+
+            };
+
+        }
+    } catch (err) {
+        throw err;
+    }
+}
